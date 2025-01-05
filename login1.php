5ff9d97fc8547a1442251bbcc22ad08cfbe06ae4
Salsabilaica <salsabilaramadani80@gmail.com>
2025-01-05 08:53:08 +0700

    !

---
+++ login1.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Perpusku</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="navbar">
+      <div class="left">
+        <a href="#"> Home </a>
+        <a href="#"> Buku </a>
+        <a href="#"> Tentang </a>
+        <a href="#"> Kontak Kami </a>
+      </div>
+      <div class="right">
+        <a href="login.php"> Login </a>
+        <a href="register.php"> Register </a>
+      </div>
+    </div>
+    <div class="container">
+      <div class="library">
+        <h2>Selamat Datang!</h2>
+        <p>Menyediakan berbagai koleksi buku terbaru</p>
+      </div>
+      <div class="pos">
+        <div class="card">
+          <div class="card-image">
+            <img src="bumi.jpg" alt="Judul Buku" />
+          </div>
+          <div class="card-details">
+            <h3 class="book-title">Bumi</h3>
+            <p class="book-details">Penulis : Tere Liya</p>
+            <p class="book-details">Tahun Terbit : 2022</p>
+          </div>
+          <div class="button">
+            <button class="btn">Pinjam</button>
+            <button class="btn">Detail</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <img src="matahari.jpg" alt="Judul Buku" />
+          </div>
+          <div class="card-details">
+            <h3 class="book-title">Matahari</h3>
+            <p class="book-details">Penulis : Tere Liya</p>
+            <p class="book-details">Tahun Terbit : 2018</p>
+          </div>
+          <div class="button">
+            <button class="btn">Pinjam</button>
+            <button class="btn">Detail</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <img src="bulan.jpg" alt="Judul Buku" />
+          </div>
+          <div class="card-details">
+            <h3 class="book-title">Bulan</h3>
+            <p class="book-details">Penulis : Tere Liye</p>
+            <p class="book-details">Tahun Terbit : 2022</p>
+          </div>
+          <div class="button">
+            <button class="btn">Pinjam</button>
+            <button class="btn">Detail</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <img src="bintang.jpg" alt="Judul Buku" />
+          </div>
+          <div class="card-details">
+            <h3 class="book-title">Bintang</h3>
+            <p class="book-details">Penulis : Tere Liye</p>
+            <p class="book-details">Tahun Terbit : 2017</p>
+          </div>
+          <div class="button">
+            <button class="btn">Pinjam</button>
+            <button class="btn">Detail</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-image">
+            <img src="laskar.jpg" alt="Judul Buku" />
+          </div>
+          <div class="card-details">
+            <h3 class="book-title">Laskar Pelangi</h3>
+            <p class="book-details">Penulis : Andrea Hirata</p>
+            <p class="book-details">Tahun Terbit : 2005</p>
+          </div>
+          <div class="button">
+            <button class="btn">Pinjam</button>
+            <button class="btn">Detail</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>&copy; Pier 2025. Hak Cipta Dilindungi.</footer>
+  </body>
+</html>
