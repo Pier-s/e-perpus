64959e4113c64b3779610aaa1dcf0d2b78236c65
Salsabilaica <salsabilaramadani80@gmail.com>
2025-01-05 16:43:03 +0700

    !

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-//------------------------------::::::::::::::::::::------------------------------\\
-// Dibuat oleh di PT. Pacifica Raya Technology \\
-//------------------------------::::::::::::::::::::------------------------------\\
+
 
 // Arahkan user ketika pertama kali membuka applikasi ke halaman login
-header("location: masuk");
+header("location: login1.php");
